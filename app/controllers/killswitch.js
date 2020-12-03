96828d69340f742f0f3acc60c4bb22390e4e72a2
Dennis Dreissen <dennisdreissen@gmail.com>
2020-12-03 10:34:10 +0100

    Add kill switch for the app, app has reached EOL in 2021. Bye.

---
+++ app/controllers/killswitch.js
@@ -0,0 +1,15 @@
+/* jshint node: true */
+/* jshint esversion: 6 */
+
+/**
+ * @ Dennis Dreissen
+ * @ Licensed under the GNU General Public License v3.0. See the LICENCE file for more information.
+ */
+
+'use strict';
+
+module.exports = {
+    get: (request, reply) => {
+        reply("");
+    }
+}
